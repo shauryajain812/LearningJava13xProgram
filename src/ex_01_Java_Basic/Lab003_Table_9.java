04cfb7a5ca3fd6a3785b80dc83157cdd98ed0cf4
LAPTOP-8JB3GR11\Shaurya <shaurya.jain@avikalsolutions.com>
2025-06-11 01:58:33 +0530

    New program for table of 9

---
+++ src/ex_01_Java_Basic/Lab003_Table_9.java
@@ -0,0 +1,28 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Table_9 {
+    public static void main(String[] args) {
+        int a=9;
+        int b=1;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+        b++;
+        System.out.printf("%d x %d = %d\n",a,b,a*b);
+
+    }
+}
